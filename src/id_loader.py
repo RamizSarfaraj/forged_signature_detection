1ff373073bc38a01dfef22594094f1347b0710e3
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-12 11:26:12 +0530

    experimenting

--- src/id_loader.py
+++ src/id_loader.py
@@ -6,50 +6,19 @@ import cv2
 import os
 
 
-def find_id(path):
-    """
-    Traverse throught the given folder and subfolders, fine the full path of the image.
-    """
-    id_lst = []
-    for root, dirs, filename in os.walk(path):
-        for _d in dirs:
-            path = os.path.join(root, _d)
-            for file in os.listdir(path):
-                file_id = os.path.join(path, file)
-                if os.path.isdir(file_id):
-                    for d_ in os.listdir(file_id):
-                        new_path = os.path.join(file_id, d_)
-                        id_lst.append(new_path)
-                else:
-                    id_lst.append(file_id)
-    return id_lst
+def id_loader(path):
+    id_list = []
+    for dirname, _, filenames in os.walk(path):
+        for file in filenames:
+            im = os.path.join(dirname, file)
+            if im is not None:
+                id_list.append(im)
+
+    return id_list
 
 
-English_path = "./../Signs/Processed-Original/English/"
+Original = "/home/ramiz/Signature_project/Signs/Processed-Original/"
 
-Eng_id = find_id(path=English_path)
+id_list = id_loader(Original)
 
-def labeller(id_list):
-    lst, lab = [], []
-    for im in id_list:
-        img = im
-        lb = 1
-        if img is not None:
-            lst.append(img)
-            lab.append(lb)
-    return lst, lab
-
-
-id_s, lab = labeller(Eng_id)
-
-
-data = {
-        'ids': id_s,
-        'labell': lab
-        }
-
-df = pd.DataFrame(data)
-
-print(df.shape)
-
-print(df.head())
+print(len(id_list))
